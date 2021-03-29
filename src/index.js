6c4b7129a8396e9ab35a958fb24cb8527416d8d3
Cassandra Parsons <parsons0006@gmail.com>
2021-03-29 11:24:33 -0400

    links

--- src/index.js
+++ src/index.js
@@ -1,23 +1,47 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Switch, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Link, Redirect } from 'react-router-dom'
 import Router from './utils/Router'
-import { AuthUserProvider } from './utils/AuthUser'
-import AuthorizedRoute from './utils/AuthorizedRoute'
-import UnauthorizedLayout from './layouts/UnauthorizedLayout'
-import AuthorizedLayout from './layouts/AuthorizedLayout'
+// import { AuthUserProvider } from './utils/AuthUser'
+// import AuthorizedRoute from './utils/AuthorizedRoute'
+// import UnauthorizedLayout from './layouts/UnauthorizedLayout'
+// import AuthorizedLayout from './layouts/AuthorizedLayout'
 import './styles/main.scss'
 
-const App = () => (
-  <Router>
-    <AuthUserProvider>
+function Home() {
+  return <h1>Home Page</h1>;
+}
+
+function Contact() {
+  return <h1>Contact</h1>;
+}
+
+const App = () => {
+  return (
+  <BrowserRouter>
+    <div>
+      <nav>
+        <Link to="/">Home</Link> <Link to="/contact">Contact</Link>
+      </nav>
       <Switch>
-        <Route path="/auth" component={UnauthorizedLayout} />
-        <AuthorizedRoute path="/projects" component={AuthorizedLayout} />
-        <Redirect to="/projects" />
+        <Route exact path="/" component={Home} />
+        <Route path="/contact" component={Contact} />
       </Switch>
-    </AuthUserProvider>
-  </Router>
-)
+    </div>
+  </BrowserRouter>
+  )
+}
+
+// const App = () => (
+//   <Router>
+//     <AuthUserProvider>
+//       <Switch>
+//         <Route path="/auth" component={UnauthorizedLayout} />
+//         <AuthorizedRoute path="/projects" component={AuthorizedLayout} />
+//         <Redirect to="/projects" />
+//       </Switch>
+//     </AuthUserProvider>
+//   </Router>
+// )
 
 ReactDOM.render(<App />, document.getElementById('root'))
